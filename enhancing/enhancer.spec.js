beb94fc0122aa76a81aff19e69f70fc29d8612bf
Austin Walela <austinmasibo@gmail.com>
2020-03-26 06:19:36 +0300

    enhancement failure tests passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -26,4 +26,39 @@ describe('enhancer methods', () => {
       ).toEqual(expectedOutput)
     })
   })
+  describe('failed enhancement method', () => {
+    const obj1 = { name: 'Scorpion', durability: 98, enhancement: 14 }
+    const obj2 = { name: 'Liu Kang', durability: 96, enhancement: 15 }
+    const obj3 = { name: 'Sub Zero', durability: 99, enhancement: 20 }
+
+    describe('given an enhancement of less than 15', () => {
+      test('returns an item with the correctly adjusted durability', () => {
+        expect(enhancer.fail(obj1)).toEqual({
+          name: 'Scorpion',
+          durability: 93,
+          enhancement: 14
+        })
+      })
+    })
+
+    describe('given an enhancement of 15 or more', () => {
+      test('returns an item with the correctly adjusted durability', () => {
+        expect(enhancer.fail(obj2)).toEqual({
+          name: 'Liu Kang',
+          durability: 86,
+          enhancement: 15
+        })
+      })
+
+      describe('given an enhancement of more than 16', () => {
+        test('returns item with correctly adjusted durability and enhancement', () => {
+          expect(enhancer.fail(obj3)).toEqual({
+            name: 'Sub Zero',
+            durability: 89,
+            enhancement: 19
+          })
+        })
+      })
+    })
+  })
 })
